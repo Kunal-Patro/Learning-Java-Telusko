a26365a6e39979c549d226a6929a1071e48d8ebc
Kunal Sharma <kunal0199@gmail.com>
2022-01-27 21:04:05 +0530

    Super method done

---
+++ JAVA_Basics/Super.java
@@ -0,0 +1,40 @@
+/*
+ * Super method calls the parent constructor based on parameters passed to it.
+ * Super method is called automatically by the sub class's constructor even if not mentioned.
+ * Super method call should be first statement of the constructor, so there can only be one super method call in constructor at a time.
+ * Parameters of super method call should match one of the parent's constructor.
+ */
+class A
+{
+	public A()
+	{
+		System.out.println("I am in A(Default)");
+	}
+	public A(int a)
+        {
+                System.out.println("I am in A(int)");
+        }
+
+}
+class B extends A
+{
+	public B()
+        {
+		super();
+		// super(30); This will call A's parametrized constructor
+                System.out.println("I am in B(Default)");
+        }
+	public B(int a)
+       	{
+		super(a);
+               	System.out.println("I am in B(int)");
+        }
+
+}
+public class Super
+{
+	public static void main(String[] args)
+	{
+		B obj = new B();
+	}
+}
