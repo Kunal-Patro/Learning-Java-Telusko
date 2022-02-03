7b0500b8ef576b883372de68b7090f40a5e119f8
Kunal Sharma <kunal0199@gmail.com>
2022-02-03 22:15:51 +0530

    Super.java file comments updated

--- JAVA_Basics/Super.java
+++ JAVA_Basics/Super.java
@@ -3,6 +3,7 @@
  * Super method is called automatically by the sub class's constructor even if not mentioned.
  * Super method call should be first statement of the constructor, so there can only be one super method call in constructor at a time.
  * Parameters of super method call should match one of the parent's constructor.
+ * Super keyword is used to refer to the parent class members.
  */
 class A
 {
