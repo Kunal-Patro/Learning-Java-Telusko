49cb1b3eeb914e655d642f3590aa8a859c89d1b7
Kunal Sharma <kunal0199@gmail.com>
2022-02-14 20:45:11 +0530

    Functional Interface done

---
+++ JAVA_Basics/FunctionalInterface.java
@@ -0,0 +1,19 @@
+/*
+ * Types of interfaces:
+ * 1. Normal - have more than one methods.
+ * 2. Single abstract/ Functional - have only one method. Lambda Expression can be used to define.
+ * 3. Marker - doesn't have any method.
+ * As functional interface contains only one function so we can use anonymous class to define it without using "public and return datatype" of the function and "new with interface name" while object creation.
+ */
+interface A
+{
+	void show();
+}
+public class FunctionalInterface
+{
+	public static void main(String[] args)
+	{
+		A obj = ()-> {System.out.println("In Functional Interface");}; // Lambda Expression
+		obj.show();
+	}
+}
