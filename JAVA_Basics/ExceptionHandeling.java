964842987d90509c728e4d1525c0c015962a7396
Kunal Sharma <kunal0199@gmail.com>
2022-02-17 22:43:06 +0530

    Exception Handeling of checked errors done

--- JAVA_Basics/ExceptionHandeling.java
+++ JAVA_Basics/ExceptionHandeling.java
@@ -8,13 +8,21 @@
  * Catch block catches the exception and execute the statements within and continues execution. 
  * Finally block is executed everytime regardless of whether there is an exception or not.
  * We can have multiple catch blocks and they are checked in the order they are coded.
+ * Try block needs either catch block or finally block otherwise it will give error.
+ * If we are using any resource and exception occures there, we apply try, finally block to release the resource if exception occures. We can instead use try block with resouce allocation only for that.
  */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
 public class ExceptionHandeling
 {
 	public static void main(String[] args)
 	{
 		int i,j,k;
-		int a[] = new int[5];	
+		int a[] = new int[5];
+		ExceptionHandeling eh = new ExceptionHandeling();	
 		try
 		{
 			i = 10;
@@ -22,6 +30,7 @@ public class ExceptionHandeling
 			a[4] = 10;
 			k = i/j;
 			System.out.println("Answer is " + k);
+			eh.Input();
 		}
 
 		// catch(ArithmeticException | ArrayIndexOutOfBoundsException e)  this can written if we want to execute same set of statements under both exceptions.
@@ -39,11 +48,33 @@ public class ExceptionHandeling
 		catch(Exception e)
 		{
 			System.out.println("Something went wrong ...");
+			System.out.println(e);
 		}
 
 		finally
 		{
 			System.out.println("Tata");
+		}
+	}
+	public void Input() throws Exception
+	{
+		int n = 0;
+		System.out.println("Enter a number ");
+		BufferedReader br = null;
+		try    			//try(BufferReader br = new BufferedReader(new InputStreamReader(System.in)))
+		{
+			br = new BufferedReader(new InputStreamReader(System.in));
+			n = Integer.parseInt(br.readLine());
+			System.out.println(n);
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("Enter a valid number");
+		}
+		finally
+		{
+			br.close();
+		
 		}
 	}
 }
